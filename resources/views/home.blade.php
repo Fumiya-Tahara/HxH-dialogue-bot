9ee6db27e8ef13dcd20a8387611620e659293655
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-02-02 12:02:02 +0900

    デザイン少し

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,8 @@
 @extends('layouts.app')
 
+@section('content')
+<div class="d-flex flex-column">
+    <h1 style="text-align: center;">HxH dialogue BOT</h1>
+    <div style="text-align: center;">LINE HxH dialogue BOTの管理者用サイトです。</div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @include('components.head')
 </head>
-<body>
+<body style="height: 100vh;">
     <header>
-        <nav class="navbar navbar-dark bg-dark">
+        <nav class="navbar navbar-primary bg-primary">
         <!-- Navbar content -->
+            <a class="nav-link fs-4" href="">LINE HxH dialogue BOT</a>
         </nav>
     </header>
 
