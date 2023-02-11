229f064b1d94df8bdcbe91983e21501993a19868
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-02-11 12:10:33 +0900

    ログインID追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            // デフォルト
             $table->id();
+            $table->integer('login_id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
